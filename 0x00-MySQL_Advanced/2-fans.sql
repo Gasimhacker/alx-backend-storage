8405895410708f1b22d388c2caa24de06dd51ee9
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-11 23:55:33 +0000

    Use sum instead of count

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Count the number of fans in each county
-SELECT origin, count(fans) as nb_fans
+SELECT origin, sum(fans) as nb_fans
        FROM metal_bands
        Group by origin
        ORDER BY nb_fans DESC;
