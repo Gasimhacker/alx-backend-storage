705951253411a73a3df666dbfd6c5a2cd1570cf3
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-18 00:46:17 +0000

    Implement getter methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """A module that contains the Cache class"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,19 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        """Get the value associated with this key in the database"""
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Parametrize Cache.get with the str function"""
+        return self._redis.get(key, str).decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """Parametrize Cache.get with the int function"""
+        return self._redis.get(key, int)
