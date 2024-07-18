b0ea3586d756e0c9a258839149169a87fc322270
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-18 02:31:48 +0000

    Get back to the original code

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,21 +7,21 @@ from typing import Callable
 from datetime import timedelta
 
 
+r = redis.Redis()
+
+
 def cache(fn: Callable) -> Callable:
     """Cache the get requests"""
     @wraps(fn)
-    def wrapper(*args, **kwargs):
+    def wrapper(url):
         """A wrapper to cache the result of get equests"""
-        url = args[0]
-        r = redis.Redis()
         r.incr(f'count:{url}')
         content = r.get('content: url')
         if content:
             return content.decode('utf-8')
-        res = fn(*args, **kwargs)
-        r.set('content: url', res)
+        res = fn(url)
         r.set(f'count:{url}', 1)
-        r.expire(f'content:{url}', timedelta(seconds=10))
+        r.setex('content: url', 10, res)
         return res
     return wrapper
 
